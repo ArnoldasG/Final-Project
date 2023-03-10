2bc14f188b9e727fb102441632a0dd0fe60d7856
ArnoldasG <grevasarnoldas@gmail.com>
2023-03-10 11:14:35 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,30 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCzg1NyJ1EWiD8dt4l0KuDc3ds4mZV-NLA",
+
+  authDomain: "testiniai-51caa.firebaseapp.com",
+
+  databaseURL:
+    "https://testiniai-51caa-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "testiniai-51caa",
+
+  storageBucket: "testiniai-51caa.appspot.com",
+
+  messagingSenderId: "403030605831",
+
+  appId: "1:403030605831:web:22454e82eb866e7290eaa2",
+};
+
+export { firebaseConfig };
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA0Ri7ycY1n_jpA73QQsPZFCVzkjJ6cquM",
+//     authDomain: "testiniai-e5031.firebaseapp.com",
+//     databaseURL: "https://testiniai-e5031-default-rtdb.firebaseio.com",
+//     projectId: "testiniai-e5031",
+//     storageBucket: "testiniai-e5031.appspot.com",
+//     messagingSenderId: "152200998825",
+//     appId: "1:152200998825:web:85b6ec27b4e2078c25deb8",
+//   };
+
+//   export { firebaseConfig };
